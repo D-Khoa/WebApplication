d4caddb848cf9c32036c8427e0b16849c8ee34d5
DangKhoa <dk.wofts@gmail.com>
2019-11-25 14:00:34 +0700

    WebDemoV2

---
+++ WebDemoV2/WebDemoV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDemoV2.Startup))]
+namespace WebDemoV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
